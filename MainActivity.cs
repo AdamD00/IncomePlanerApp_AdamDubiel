0f3b0f2338da07e5f04e53e9e76f5438321238f8
Mrokon <mrokon00@gmail.com>
2023-06-18 08:41:12 +0200

    All App

---
+++ MainActivity.cs
@@ -0,0 +1,99 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+
+namespace App2
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        EditText incomePerHourEditText;
+        EditText workHourPerDayEditText;
+        EditText taxRateEditText;
+        EditText savingRateEditText;
+
+        TextView workSummaryTextView;
+        TextView grossIncomeTextView;
+        TextView taxPayableTextView;
+        TextView annaulSavingsTextView;
+        TextView spendableIncomeTextView;
+
+        Button calculateButton;
+        RelativeLayout resultLayout;
+
+        bool inputCalculated = false;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            
+            SetContentView(Resource.Layout.activity_main);
+            ConnectViews();
+            
+
+
+        }
+
+        void ConnectViews()
+        {
+            incomePerHourEditText = FindViewById<EditText>(Resource.Id.incomePerHourEditText);
+            workHourPerDayEditText = FindViewById<EditText>(Resource.Id.workHourEditText);
+            taxRateEditText = FindViewById<EditText>(Resource.Id.taxRateEditText);
+            savingRateEditText = FindViewById<EditText>(Resource.Id.savingsRateEditText);
+
+            workSummaryTextView = FindViewById<TextView>(Resource.Id.workSummaryTextView);
+            grossIncomeTextView = FindViewById<TextView>(Resource.Id.grossIncomeTextView);
+            taxPayableTextView = FindViewById<TextView>(Resource.Id.taxPayableTextView);
+            annaulSavingsTextView = FindViewById<TextView>(Resource.Id.savingsTextView);
+            spendableIncomeTextView = FindViewById<TextView>(Resource.Id.spendableIncomeTextView);
+
+            calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+            resultLayout = FindViewById<RelativeLayout>(Resource.Id.resultLayout);
+
+            calculateButton.Click += CalculateButtonClick;
+
+        }
+        private void CalculateButtonClick(object senderm, System.EventArgs e)
+        {
+            if (inputCalculated)
+            {
+                inputCalculated = false;
+                calculateButton.Text = "Calculate";
+                ClearInput();
+                return;
+            }
+            double incomePerHour = double.Parse(incomePerHourEditText.Text);
+            double workHourPerDay = double.Parse(workHourPerDayEditText.Text);
+            double taxRate = double.Parse(taxRateEditText.Text);
+            double savingsRate = double.Parse(savingRateEditText.Text);
+
+            double annualWorkHourSummary = workHourPerDay * 5 * 50;
+            double annualIncome = incomePerHour * workHourPerDay * 5 * 50;
+            double taxPayable = (taxRate / 100) * annualIncome;
+            double annualSavings = (savingsRate / 100) * annualIncome;
+            double spendableIncome = annualIncome - annualSavings - taxPayable;
+
+            grossIncomeTextView.Text = annualIncome.ToString("#,##") + " USD";
+            workSummaryTextView.Text = annualWorkHourSummary.ToString("#,##") + " HRS";
+            taxPayableTextView.Text = taxPayable.ToString("#,##") + " USD";
+            annaulSavingsTextView.Text = annualSavings.ToString("#,##") + " USD";
+            spendableIncomeTextView.Text = spendableIncome.ToString("#,##") + " USD";
+
+            resultLayout.Visibility = Android.Views.ViewStates.Visible;
+            inputCalculated = true;
+            calculateButton.Text = "Clear";
+        }
+
+        private void ClearInput()
+        {
+            incomePerHourEditText.Text="";
+            workHourPerDayEditText.Text="";
+            taxRateEditText.Text="";
+            savingRateEditText.Text="";
+        }
+
+
+    }
+}
